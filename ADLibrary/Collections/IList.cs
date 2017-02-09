dec6888de14c34da5a6bee09ad6baca964761e54
Wouter Brookhuis <wouter.brookhuis@student.stenden.com>
2017-02-09 10:54:24 +0100

    Project upload

---
+++ ADLibrary/Collections/IList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLibrary.Collections
+{
+    public interface IList<T>
+    {
+        void add(T item);
+        T get(int index);
+        int indexOf(T item);
+        void remove(int index);
+        int count();
+    }
+}
