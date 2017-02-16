a03ec8da998d8c38f72ee5a204e74297dbf2cca0
Bram Kelder <bram-kelder@hotmail.com>
2017-02-16 13:46:27 +0100

    SelectionSort

---
+++ ADLibrary/Sorting/SelectionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLibrary.Sorting
+{
+    class SelectionSort : ISorter
+    {
+        public void sort(IComparable[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int indexSmallest = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j].CompareTo(array[indexSmallest]) < 0)
+                    {
+                        indexSmallest = j;
+                    }
+
+                    IComparable temp = array[i];
+                    array[i] = array[indexSmallest];
+                    array[indexSmallest] = temp;
+                }
+            }
+        }
+    }
+}
