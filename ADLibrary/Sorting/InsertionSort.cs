95c017eceae0338ba18e605f9fd360592d9ca2e4
Bram Kelder <bram-kelder@hotmail.com>
2017-02-16 14:37:57 +0100

    InsertionSort

---
+++ ADLibrary/Sorting/InsertionSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLibrary.Sorting 
+{
+    class InsertionSort
+    {
+        public static void sort<T>(T[] array) where T : IComparable
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
