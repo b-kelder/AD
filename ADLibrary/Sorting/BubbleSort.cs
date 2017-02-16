e6ab45ef3ac0aa9f7995fa4268b1b2402390ee61
Bram Kelder <bram-kelder@hotmail.com>
2017-02-16 14:06:34 +0100

    comments

--- ADLibrary/Sorting/BubbleSort.cs
+++ ADLibrary/Sorting/BubbleSort.cs
@@ -8,19 +8,23 @@ namespace ADLibrary.Sorting
 {
     public class BubbleSort : ISorter
     {
+        /// <summary>
+        /// Method that sorts and IComparable array using the bubble sort algorithm.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
         public void sort(IComparable[] array)
         {
-            IComparable temp;
+            IComparable temp;                                   //Create a variable to temporary save an item.
 
-            for (int i = array.Length - 1; i >= 1; i--)
+            for (int i = array.Length - 1; i >= 1; i--)         //loop door iets? !@##$!$%@%^$
             {
-                for (int j = 0; j < array.Length; j++)
+                for (int j = 0; j < array.Length; j++)          //loop door iets? !@##$!$%@%^$
                 {
-                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    if (array[j].CompareTo(array[j + 1]) > 0)   //Check if the current item is larger than next item in the array.
                     {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
+                        temp = array[j];                        //If so, temporary save the current item.
+                        array[j] = array[j + 1];                //Give the smaller of the two the index of the larger one.
+                        array[j + 1] = temp;                    //Give the larger item the index of the smaller one, basicly swapping them.
                     }
                 }
             }
--- ADLibrary/Sorting/SelectionSort.cs
+++ ADLibrary/Sorting/SelectionSort.cs
@@ -8,22 +8,27 @@ namespace ADLibrary.Sorting
 {
     class SelectionSort : ISorter
     {
+        /// <summary>
+        /// Method that sorts and IComparable array using the selection sort algorithm.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
         public void sort(IComparable[] array)
         {
-            for (int i = 0; i < array.Length - 1; i++)
+            for (int i = 0; i < array.Length - 1; i++)                      //Loop through all items in the array.
             {
-                int indexSmallest = i;
-                for (int j = i + 1; j < array.Length; j++)
+                int indexSmallest = i;                                      //Save the index of the smallest item in the array.
+
+                for (int j = i + 1; j < array.Length; j++)                  //Loop through all the unsorted items (everything up to i has been sorted).
                 {
-                    if (array[j].CompareTo(array[indexSmallest]) < 0)
+                    if (array[j].CompareTo(array[indexSmallest]) < 0)       //Check if the current item is smaller than the currently know smallest item.
                     {
-                        indexSmallest = j;
+                        indexSmallest = j;                                  //If so, save the item as the new smallest item.
                     }
                 }
 
-                IComparable temp = array[i];
-                array[i] = array[indexSmallest];
-                array[indexSmallest] = temp;
+                IComparable temp = array[i];                                //After finding the new smallest item, temporary save the next unsorted item.
+                array[i] = array[indexSmallest];                            //Put the smallest item on its new index.
+                array[indexSmallest] = temp;                                //Put the temporary saved item back in the array, basicly swapping them.
             }
         }
     }
