48843cf752feade6578a00743296007e8dba0178
Bram Kelder <bram-kelder@hotmail.com>
2017-03-02 14:01:58 +0100

    Extra comments

--- ADLibrary/Sorting/InsertionSort.cs
+++ ADLibrary/Sorting/InsertionSort.cs
@@ -8,6 +8,10 @@ namespace ADLibrary.Sorting
 {
     class InsertionSort
     {
+        /// <summary>
+        /// Method that sorts and IComparable array using the insertion sort algorithm.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
         public static void sort<T>(T[] array) where T : IComparable
         {
             int length = array.Length;
--- ADLibrary/Sorting/SmartBubbleSort.cs
+++ ADLibrary/Sorting/SmartBubbleSort.cs
@@ -8,6 +8,10 @@ namespace ADLibrary.Sorting
 {
     public static class SmartBubbleSort
     {
+        /// <summary>
+        /// Method that sorts and IComparable array using a smart bubble sort algorithm.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
         public static void sort<T>(T[] array) where T : IComparable
         {
             int length = array.Length;
