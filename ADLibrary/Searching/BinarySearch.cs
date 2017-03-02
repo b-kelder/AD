24ae164b4553ce783bb563a5b245844b8efa9381
Bram Kelder <bram-kelder@hotmail.com>
2017-03-02 13:34:07 +0100

    Sorting

---
+++ ADLibrary/Searching/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLibrary.Searching
+{
+    class BinarySearch
+    {
+        public static void search<T>(T[] array, T item) where T : IComparable
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            int midpoint;
+
+            while (low <= high)
+            {
+                midpoint = low + (high - low) / 2;
+
+                if (item.CompareTo(array[midpoint]) == 0)
+                    //TODO: ITEM FOUND
+                    throw new NotImplementedException();
+                else if (item.CompareTo(array[midpoint]) < 0)
+                    high = midpoint - 1;
+                else
+                    low = midpoint + 1;
+            }
+
+            //TODO: NOT FOUND
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ADLibrary/Searching/MinMaxSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLibrary.Searching
+{
+    class MinMaxSearch
+    {
+        public static void search<T>(T[] array, T item) where T : IComparable
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
