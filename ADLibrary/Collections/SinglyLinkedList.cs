61e06a9d861e0caa1d724c1522e8d48ae2f9e3f2
Bram Kelder <bram-kelder@hotmail.com>
2017-03-09 19:07:22 +0100

    linkedlist start

---
+++ ADLibrary/Collections/SinglyLinkedList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLibrary.Collections
+{
+    public class SinglyLinkedList<T> : ICollection<T>
+    {
+        private Node<T> next;
+        private Node<T> head;
+
+        public SinglyLinkedList()
+        {
+
+        }
+
+        public void add(T element)
+        {
+            Node<T> toAdd = new Node<T>();
+            toAdd.element = element;
+            Node<T> current = head;
+            while (current != null)
+            {
+                current = current.next;
+            }
+            current.next = toAdd;
+        }
+
+        public void clear()
+        {
+            head = null;
+        }
+
+        public bool contains(T item)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.element.Equals(item))
+                {
+                    return true;
+                }
+                current = current.next;
+            }
+            return false;
+        }
+
+        public int count()
+        {
+            int count = 0;
+            Node<T> current = head;
+            while (current != null)
+            {
+                current = current.next;
+                count++;
+            }
+            return count;
+        }
+
+        public T[] toArray()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class Node<T>
+    {
+        public Node<T> next { get; set; }
+        public T element { get; set; }
+
+        public Node()
+        {
+
+        }
+    }
+}
