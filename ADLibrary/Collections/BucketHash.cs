ec45381bcbde5466dc130c2465ffec7067bb14db
Wouter Brookhuis <wouter.brookhuis@student.stenden.com>
2017-03-17 11:47:29 +0100

    Quadra Hash, comments and Exceptions

--- ADLibrary/Collections/BucketHash.cs
+++ ADLibrary/Collections/BucketHash.cs
@@ -1,5 +1,9 @@
 ﻿namespace ADLibrary.Collections
 {
+    /// <summary>
+    /// Bucket hash demo class.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class BucketHash<T>
     {
         const int SIZE = 101;
@@ -14,26 +18,39 @@
             }
         }
 
-        public int hash(T key)
+        /// <summary>
+        /// Hashes a key and returns a valid array index.
+        /// </summary>
+        /// <param name="key">The key to hash</param>
+        /// <returns>Index for key</returns>
+        private int hash(T key)
         {
             var i = key.GetHashCode();
             i = i % buckets.Length;
             if(i < 0)
             {
-                i += buckets.Length;
+                i += buckets.Length;                                // Ensure the result is positive
             }
             return i;
         }
 
+        /// <summary>
+        /// Inserts an item in the collection.
+        /// </summary>
+        /// <param name="item">The item to insert</param>
         public void insert(T item)
         {
             var hashValue = hash(item);
-            if(!buckets[hashValue].contains(item))
+            if(!buckets[hashValue].contains(item))                  // Only add it if it's not already stored in there
             {
                 buckets[hashValue].add(item);
             }
         }
 
+        /// <summary>
+        /// Removes an item from the collection.
+        /// </summary>
+        /// <param name="item">The item to remove.</param>
         public void remove(T item)
         {
             var hashValue = hash(item);
--- ADLibrary/Collections/QuadraticHash.cs
+++ ADLibrary/Collections/QuadraticHash.cs
@@ -6,9 +6,14 @@ using System.Threading.Tasks;
 
 namespace ADLibrary.Collections
 {
-    public class QuadraticHash<T>
+    /// <summary>
+    /// Quadratic Hash demo.
+    /// TODO: Find a way to deal with non-nullable types.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class QuadraticHash<T> where T : IComparable
     {
-        const int SIZE = 101;               // Should be prime?
+        const int SIZE = 101;               // Size of the hashtable
 
         T[] hashtable;
 
@@ -18,29 +23,77 @@ namespace ADLibrary.Collections
         }
 
         /// <summary>
-        /// Returns GetHashCode of key modulo SIZE.
+        /// Takes a hash and ensures it's a valid array index
         /// </summary>
-        /// <param name="key">The key to hash</param>
-        /// <returns>Positive array index</returns>
-        public int hash(T key)
+        /// <param name="hash"></param>
+        /// <returns></returns>
+        private int normalizeHash(int hash)
         {
-            var i = key.GetHashCode();
-            i = i % SIZE;
-            if(i < 0)
+            hash = hash % SIZE;
+            if(hash < 0)
             {
-                i += SIZE;
+                hash += SIZE;
             }
-            return i;
+            return hash;
+        }
+
+        /// <summary>
+        /// Removes the key from the collection.
+        /// </summary>
+        /// <param name="key">The key to remove</param>
+        public void remove(T key)
+        {
+            var index = indexOf(key);
+            if(index > 0)
+            {
+                hashtable[index] = default(T);
+            }
+        }
+
+        /// <summary>
+        /// Returns the index of key in hashtable.
+        /// -1 is returned when key can not be found.
+        /// </summary>
+        /// <param name="key">The key to find</param>
+        /// <returns>Index of key in hashtable</returns>
+        private int indexOf(T key)
+        {
+            var colCount = 0;                                           // Stores the amount of collisions we had
+            var normalizedHash = normalizeHash(key.GetHashCode());
+            while(colCount < SIZE)
+            {
+                if(hashtable[normalizedHash] != null &&
+                    hashtable[normalizedHash].Equals(key))              // Check if spot is not empty and the key is stored here
+                {
+                    return normalizedHash;                              // Key was found, return index
+                }
+                else
+                {
+                    colCount++;
+                    normalizedHash = normalizeHash(key.GetHashCode() + colCount * colCount);    // Get next index by applying quadratic hash function
+                }
+            }
+            return -1;                                                  // Key not found
         }
 
         public void insert(T key)
         {
-            var j = 0;
-            var normalizedHash = hash(key);
-            if(hashtable[normalizedHash] == null)                   // Check if spot is empty
+            var colCount = 0;                                           // Stores the amount of collisions we had
+            var normalizedHash = normalizeHash(key.GetHashCode());
+            while(colCount < SIZE)
             {
-
+                if(hashtable[normalizedHash] == null)                   // Check if spot is empty
+                {
+                    hashtable[normalizedHash] = key;                    // Add an return
+                    return;
+                }
+                else
+                {
+                    colCount++;
+                    normalizedHash = normalizeHash(key.GetHashCode() + colCount * colCount);    // Get next index by applying quadratic hash function
+                }
             }
+            throw new HashTableFullException();                         // Hash table is full if j >= SIZE
         }
     }
 }
