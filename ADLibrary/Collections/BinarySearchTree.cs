2fb66ad89ba36275cdba2a0ff3dc24392162f564
Bram Kelder <bram-kelder@hotmail.com>
2017-03-23 15:42:14 +0100

    Fix visual studio error

--- ADLibrary/Collections/BinarySearchTree.cs
+++ ADLibrary/Collections/BinarySearchTree.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ADLibrary.Collections
 {
-    class BinarySearchTree
+    class BinarySearchTree<T> : ICollection<T>
     {
+        private TreeNode<T> root;
+
+        public BinarySearchTree() { }
+
+        public void add(T item)
+        {
+
+        }
+
+        public void remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] toArray()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class TreeNode<T>
+    {
+        public T data { get; set; }
+        public TreeNode<T> left { get; set; }   //Kleiner
+        public TreeNode<T> right { get; set; }  //Groter
+
+        public TreeNode(T data)
+        {
+            this.data = data;
+        }
     }
 }
