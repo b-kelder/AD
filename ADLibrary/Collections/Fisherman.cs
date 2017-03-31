e39329b87927acc2fd912f893c967aae9e4af668
Bram Kelder <bram-kelder@hotmail.com>
2017-03-31 11:07:00 +0200

    Added fisherman

---
+++ ADLibrary/Collections/Fisherman.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADLibrary.Collections
+{
+    class Fisherman : IComparable<Fisherman>
+    {
+        string firstName;
+        string lastName;
+        int age;
+        int lenghtLargestFishEverCaught;
+        bool ownsFishingBoat;
+
+        public Fisherman() { }
+
+        public Fisherman(string firstName, string lastName, int age, int lenghtLargestFishEverCaught, bool ownsFishingBoat)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.lenghtLargestFishEverCaught = lenghtLargestFishEverCaught;
+            this.ownsFishingBoat = ownsFishingBoat;
+        }
+
+        public int CompareTo(Fisherman other)
+        {
+            if (lenghtLargestFishEverCaught > other.lenghtLargestFishEverCaught)
+            {
+                return 1;
+            }
+            else if (lenghtLargestFishEverCaught < other.lenghtLargestFishEverCaught)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
